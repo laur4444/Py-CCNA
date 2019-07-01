1d6bcac453d3e6b6e780364526aee794d76030bd
Cebere Tudor <tudorcebere@gmail.com>
2019-07-01 16:23:49 +0300

    adding bonus

---
+++ 2019-summer/labs/lab04/bonus/ex8_bonus.py
@@ -0,0 +1,10 @@
+"""
+    Scrieti un program simplu in C/copiati de pe internet un program simplu in C in directorul curent (hello world).
+    Gasiti automat (fara hardcodari) sursa (presupunem ca e unica, dar would be nice sa mearga pentru mai multe).
+    Compilati sursa, rulati executabilul, si salvati output-ul intr-un fisier, totul din python.
+
+    Hint:
+        subprocess
+"""
+
+import subprocess
